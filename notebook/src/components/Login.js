f8d5c8a61eeeac55be2db1ba0e61f345a44c1910
Aditya Priyanshu <106824520+Aditya8315@users.noreply.github.com>
2022-06-04 12:05:20 +0530

    Add files via upload

---
+++ notebook/src/components/Login.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+const Login = (props) => {
+    const [credentials, setCredentials] = useState({email: "", password: ""})
+    let history =useHistory();
+    const handleSubmit=async (e)=>{
+        e.preventDefault();
+        const response = await fetch("http://localhost:5000/api/auth/login", {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+      body: JSON.stringify({email:credentials.email,password:credentials.password})
+          });
+          const json = await response.json();
+          console.log(json);
+        if(json.success){
+          localStorage.setItem('token',json.authtoken);
+          history.push('/');
+          props.showAlert("Logged In","success");
+        }
+        else{
+          props.showAlert("Invalid email or password","danger");
+        }
+    }
+    const onChange = (e)=>{
+        setCredentials({...credentials, [e.target.name]: e.target.value})
+    }
+  return (
+    <div>
+      <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+  <div className="mb-3">
+    <label htmlFor="email" className="form-label">Email address</label>
+    <input type="email" className="form-control" onChange={onChange} value={credentials.email} id="email" name="email" aria-describedby="emailHelp"/>
+  </div>
+  <div className="mb-3">
+    <label htmlFor="password" className="form-label">Password</label>
+    <input type="password" className="form-control" onChange={onChange} value={credentials.password} id="password" name="password" />
+  </div>
+  <button type="submit" className="btn btn-primary" >Submit</button>
+</form>
+    </div>
+  )
+}
+
+export default Login
